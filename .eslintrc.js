29089ae98867bf748467eee734522b00597a4636
georapbox <georapbox@gmail.com>
2019-10-26 22:31:18 +0300

    edit eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,15 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es6: true,
-    mocha: true
-  },
-  parserOptions: {
-    ecmaVersion: 9,
-    sourceType: 'module',
-    allowImportExportEverywhere: true
+    es2020: true,
+    jest: true
   },
   extends: [
     'eslint:recommended'
   ],
   rules: {
     'indent': ['warn', 2, { 'SwitchCase': 1 }],
-    'eqeqeq': ['error', 'always', { 'null': 'ignore' }]
+    'eqeqeq': ['error', 'always', { 'null': 'ignore' }],
+    'semi': ['warn', 'always']
   }
 };

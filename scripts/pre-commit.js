434f0b1cefc259894a65ec91ee53debcf89c553d
georapbox <georapbox@gmail.com>
2019-12-10 22:26:16 +0200

    remove pre-commit hook

--- scripts/pre-commit.js
+++
@@ -1,3 +0,0 @@
-console.log('----------------------------------------------');
-console.log('Do not forget to push compiled: $npm run build');
-console.log('----------------------------------------------');
